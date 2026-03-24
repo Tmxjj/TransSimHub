c9556cc2f3ec70c047528ad9ca08152a198d5d4c
seu_jiyufei <2944557556@qq.com>
2026-03-24 11:46:21 +0800

    提高分辨率和渲染的抗锯齿等参数来提高画质质量；画质质感无法进行投影，放弃，故而现在仍没有投影

--- tshub/tshub_env3d/vis3d_renderer/rendering_components/scene_sync.py
+++ tshub/tshub_env3d/vis3d_renderer/rendering_components/scene_sync.py
@@ -2,7 +2,7 @@
 @Author: WANG Maonan
 @Date: 2024-07-13 20:53:01
 @Description: 场景的同步, 根据 SUMO 的信息更新 panda3d
-LastEditTime: 2026-03-24 00:13:21
+LastEditTime: 2026-03-24 11:45:24
 '''
 import math
 from loguru import logger
@@ -59,7 +59,7 @@ class SceneSync(object):
             'SQUARE_512': (512, 512), # 自定义 1:1 长宽比
             'SQUARE_1024': (1024, 1024), # 自定义 1:1 长宽比
             'SQUARE_2048': (2048, 2048), # 自定义 1:1 长宽比
-            'SQUARE_4096': (4096, 4096) # 自定义 1:1 长宽比
+            'SQUARE_4096': (4096, 4096) # 自定义 1:1 长宽比 
         }
         if preset not in presets:
             raise ValueError(f"Invalid preset: {preset}. Valid presets are: {list(presets.keys())}")
